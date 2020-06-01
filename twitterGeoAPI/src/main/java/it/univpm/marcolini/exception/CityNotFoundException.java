5cebc8d41429946b70f8da2388c007e62dc6bdee
Alessandro Marcolini <s1087318@studenti.univpm.it>
2020-06-01 11:08:15 +0200

    Aggiunta classe Metadata per restituire i metadati
    Aggiunta CityNotFoundException

---
+++ twitterGeoAPI/src/main/java/it/univpm/marcolini/exception/CityNotFoundException.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package it.univpm.marcolini.util.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+/**
+ * @author Alessandro Marcolini
+ * @version 1.0
+ *
+ */
+@SuppressWarnings("serial")
+public class CityNotFoundException extends ResponseStatusException{
+	
+	/**
+	 * 
+	 * @param status the http error code
+	 * @param message the message to display
+	 */
+	public CityNotFoundException(HttpStatus status, String message) {
+		super(status, message);
+	}
+}
---
+++ twitterGeoAPI/src/main/java/it/univpm/marcolini/model/Metadata.java
@@ -0,0 +1,57 @@
+package it.univpm.marcolini.model;
+
+public class Metadata {
+	
+	protected String alias;
+	protected String attribute;
+	protected String type;
+	
+	/**
+	 * @return the alias
+	 */
+	public String getAlias() {
+		return alias;
+	}
+
+	/**
+	 * @param alias the alias to set
+	 */
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+
+	/**
+	 * @return the attribute
+	 */
+	public String getAttribute() {
+		return attribute;
+	}
+
+	/**
+	 * @param attribute the attribute to set
+	 */
+	public void setAttribute(String attribute) {
+		this.attribute = attribute;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Metadata(String alias, String attribute, String type) {
+		this.alias = alias;
+		this.attribute = attribute;
+		this.type = type;
+	}
+	
+}
